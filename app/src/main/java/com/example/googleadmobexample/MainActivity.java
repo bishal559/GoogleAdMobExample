79d2dfca6e834eecd42eccbabf653e1d5f710d59
bishal adhikari <bishaladhikari668@gmail.com>
2020-06-01 13:26:19 +0545

    new

---
+++ app/src/main/java/com/example/googleadmobexample/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.googleadmobexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+
+public class MainActivity extends AppCompatActivity {
+    EditText username,password;
+    Button loginbtn;
+    private AdView mAdView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        username=(EditText)findViewById(R.id.username);
+        password=(EditText)findViewById(R.id.password);
+        loginbtn=(Button)findViewById(R.id.loginbtn);
+        MobileAds.initialize(this,"ca-app-pub-3940256099942544~3347511713");
+//        MobileAds.initialize(this, new OnInitializationCompleteListener() {
+//            @Override
+//            public void onInitializationComplete(InitializationStatus initializationStatus) {
+//            }
+//        });
+        mAdView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+    }
+}
